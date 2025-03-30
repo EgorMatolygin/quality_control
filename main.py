9a34bc77c8f4aac4f99b435a7a044952d5b0c5e2
EgorMatolygin <egormatolygin@gmail.com>
2025-03-30 16:30:24 +0300

    Добавил заголовок

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication,QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QFont
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     
@@ -9,8 +10,15 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("App")
         self.setGeometry(600,300,500,500)
         self.setWindowIcon(QIcon('resources/icon.png'))
-    
 
+        label = QLabel("hello", self)
+        label.setFont(QFont('Georgia',40))
+        label.setGeometry(0,0,500,100)
+        label.setStyleSheet("color: #FEC4A7;"
+                            "background-color: #581845;"
+                            "border-bottom-left-radius: 30px;"
+                            "border-bottom-right-radius: 30px;")
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter )
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
