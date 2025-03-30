0cf494aadd613d4af69e2580c5cf93303ef02a7e
EgorMatolygin <egormatolygin@gmail.com>
2025-03-30 13:35:23 +0300

    Добавил иконку приложения

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
 from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     
     def __init__(self):
         super().__init__()
         self.setWindowTitle("App")
+        self.setGeometry(600,300,500,500)
+        self.setWindowIcon(QIcon('resources/icon.jpeg'))
     
 
 def main():
@@ -16,4 +19,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
