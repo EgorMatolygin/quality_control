b86458a9b1c2997441d304f4a3c865ac3d1897c8
EgorMatolygin <egormatolygin@gmail.com>
2025-05-13 11:32:24 +0300

    поправил вектор индекса качества

--- business/quality_calculator.py
+++ business/quality_calculator.py
@@ -1,22 +1,22 @@
-import numpy as np
 import pandas as pd
 
 class QualityCalculator:
     
     @staticmethod
     def calculate_quality_index(df, constraints, analysis_type="static"):
-        aggregated_params = {}
+        normalized_scores = {}
         
-        # Агрегация параметров
+        # Проход по всем параметрам с ограничениями
         for param, config in constraints.items():
             values = df[param]
             constraint_type = config['type']
-            
-            # Выбор функции агрегации
+            gamma = config.get('gamma', 1)  # Параметр из конфига
+
+            # Агрегация значений
             if constraint_type == 'range':
                 a, b = config['min'], config['max']
-                mid = (a + b) / 2
-                aggregated = values.apply(lambda p: abs(p - mid)).idxmax()
+                mid = (a + b)/2
+                aggregated = values.loc[values.apply(lambda p: abs(p - mid)).idxmax()]
                 
             elif constraint_type == 'min':
                 a = config['min']
@@ -27,51 +27,29 @@ class QualityCalculator:
                 aggregated = values.max()
                 
             elif constraint_type == 'fixed':
-                aggregated = values.all()  # Для булевых значений
+                aggregated = values.all()  # Логическое И для булевых
                 
-            aggregated_params[param] = aggregated
-
-        # Нормализация агрегированных значений
-        normalized_scores = []
-        gamma = 1  # Параметр может настраиваться
-        
-        for param, value in aggregated_params.items():
-            config = constraints[param]
-            constraint_type = config['type']
-            
+            # Нормализация
             if constraint_type == 'range':
                 a, b = config['min'], config['max']
                 c = (a + b)/2
-                if a < value < b:
-                    score = ((c - abs(value - c))**gamma) / c
+                if a < aggregated < b:
+                    score = ((c - abs(aggregated - c))**gamma) / c
                 else:
-                    score = 0
+                    score = 0.0
                     
             elif constraint_type == 'min':
                 a = config['min']
-                if value >= a:
-                    score = 1 - (1/(value - a + 1))**gamma
-                else:
-                    score = 0
-                    
+                score = 1 - (1/(aggregated - a + 1))**gamma if aggregated >= a else 0.0
+                
             elif constraint_type == 'max':
                 b = config['max']
-                if value <= b:
-                    score = 1 - (1/(1 + b - value))**gamma
-                else:
-                    score = 0
-                    
-            elif constraint_type == 'fixed':
-                score = float(value)  # Булевы в [0,1]
+                score = 1 - (1/(1 + b - aggregated))**gamma if aggregated <= b else 0.0
                 
-            normalized_scores.append(score)
+            elif constraint_type == 'fixed':
+                score = float(aggregated)  # Булевы -> 0.0/1.0
+                
+            normalized_scores[param] = score
 
-        # Интегральный индекс качества
-        if analysis_type == "static":
-            quality_index = normalized_scores
-        else:
-            # Для динамики - взвешенное среднее по времени
-            time_weights = df['timestamp'].rank()
-            quality_index = np.average(normalized_scores, weights=time_weights)
-            
-        return pd.Series([quality_index], index=['quality_index'])
+        # Возвращаем вектор нормализованных значений
+        return pd.Series(normalized_scores)
